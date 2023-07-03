238ab71e521c88feeb604b5744d155cee8ca32e9
Harisreedhar <46858047+harisreedhar@users.noreply.github.com>
2023-07-03 15:23:21 +0530

    add realesrgan face ehnhancer

---
+++ face_enhancer.py
@@ -0,0 +1,31 @@
+import os
+import torch
+import gfpgan
+from PIL import Image
+from RealESRGAN import RealESRGAN
+
+face_enhancer_list = ['NONE', 'GFPGAN', 'REAL-ESRGAN 2x', 'REAL-ESRGAN 4x', 'REAL-ESRGAN 8x']
+
+def load_face_enhancer_model(name='GFPGAN', device="cpu"):
+    if name == 'GFPGAN':
+        model_path = "./assets/pretrained_models/GFPGANv1.4.pth"
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model_path)
+        model = gfpgan.GFPGANer(model_path=model_path, upscale=1)
+    elif name == 'REAL-ESRGAN 2x':
+        model_path = "./assets/pretrained_models/RealESRGAN_x2.pth"
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model_path)
+        model = RealESRGAN(device, scale=2)
+        model.load_weights(model_path, download=False)
+    elif name == 'REAL-ESRGAN 4x':
+        model_path = "./assets/pretrained_models/RealESRGAN_x4.pth"
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model_path)
+        model = RealESRGAN(device, scale=4)
+        model.load_weights(model_path, download=False)
+    elif name == 'REAL-ESRGAN 8x':
+        model_path = "./assets/pretrained_models/RealESRGAN_x8.pth"
+        model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model_path)
+        model = RealESRGAN(device, scale=8)
+        model.load_weights(model_path, download=False)
+    else:
+        model = None
+    return model
